f5eabfabb6ce18522ae6112332d18f4b052cfc2c
tyange <usun16@gmail.com>
2023-02-25 17:59:26 +0900

    화살표 함수로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h3 className="text-red-400">image editor exam</h3>
     </div>
   );
-}
+};
 
 export default App;
