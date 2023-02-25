fb87c2da7a5ec72379fc03f50c45199a8fbed4a3
tyange <usun16@gmail.com>
2023-02-25 17:51:17 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# image-editor-exam
+
+> image editor example for React (with Typescript & Vite)
+
+- Canvas API를 활용한 이미지 편집기를 React 버전으로 구현합니다.
+- Typescript를 기반으로 하며, 개발 도구로 Vite를 사용합니다.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className="text-red-400">image editor exam</h3>
+    </div>
+  );
+}
+
+export default App;
