31ac685e1b1ac93fe4cd4673fc987e4a1e1d9291
tyange <usun16@gmail.com>
2023-02-25 18:59:15 +0900

    editor 마크업 개발

---
+++ src/components/EditorPanel.tsx
@@ -0,0 +1,13 @@
+const EditorPanel = () => {
+  return (
+    <div className="w-full flex justify-center gap-2 p-3">
+      <button>redo</button>
+      <button>undo</button>
+      <button>plus</button>
+      <button>minus</button>
+      <button>download</button>
+    </div>
+  );
+};
+
+export default EditorPanel;
