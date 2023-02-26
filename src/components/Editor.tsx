b12c38b378a7a3dcb1c5f661d05ac76227ea6ed7
tyange <usun16@gmail.com>
2023-02-26 10:00:59 +0900

    파일 업로드 기능 및 스타일링

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,72 @@
+import { useState, DragEventHandler, ChangeEventHandler } from "react";
 import EditorPanel from "./EditorPanel";
 
+import { IconFilePlus } from "@tabler/icons-react";
+
 const Editor = () => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragEnter: DragEventHandler = (e): void => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDragLeave: DragEventHandler = (e): void => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setIsDragging(false);
+  };
+
+  const handleDragOver: DragEventHandler = (e): void => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.dataTransfer!.files) {
+      setIsDragging(true);
+    }
+  };
+
+  const handleDrop: DragEventHandler = (e): void => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log(e.dataTransfer.files);
+
+    setIsDragging(false);
+  };
+
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.target.files);
+  };
+
   return (
     <div className="w-2/3 border h-4/5 rounded-md flex flex-col">
       <EditorPanel />
-      <div className="flex-1 flex justify-center items-center">
-        <canvas className="w-5/6 h-5/6 shadow-md" />
+      <div
+        className="flex-1 flex flex-col justify-center items-center"
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        <div
+          className={`w-full h-full flex flex-col gap-3 justify-center items-center`}
+        >
+          <input type="file" className="p-3" onChange={handleFileChange} />
+          <div className="relative w-full h-full flex justify-center items-center">
+            {isDragging && (
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <IconFilePlus stroke={2} size={48} />
+              </div>
+            )}
+            <canvas
+              className={`w-5/6 h-5/6 shadow-md ${
+                isDragging && "bg-slate-300"
+              }`}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
